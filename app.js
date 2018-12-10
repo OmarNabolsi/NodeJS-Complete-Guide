9e3c8a401568a1a2a26487622cab66a399807e74
Omar Nabolsi <on09@aub.edu.lb>
2018-12-10 08:53:36 +0200

    BEFORE MOVING

--- app.js
+++ app.js
@@ -60,16 +60,6 @@ app.use(errorController.get404);
 mongoose
   .connect(MONGODB_URI)
   .then(result => {
-    User.findOne().then(user => {
-      if (!user) {
-        const user = new User({
-          name: 'Omar',
-          email: 'omar@text.com',
-          cart: { items: [] }
-        });
-        user.save();
-      }
-    });
     app.listen(3000);
     console.log('Listening to http://localhost:3000');
   })
--- controllers/auth.js
+++ controllers/auth.js
@@ -16,7 +16,7 @@ exports.postLogin = (req, res, next) => {
     .then(user => {
       req.session.user = user;
       req.session.isLoggedIn = true;
-      req.session.save((err) => {
+      req.session.save(err => {
         console.log(err);
         res.redirect('/');
       });
@@ -30,5 +30,35 @@ exports.postLogout = (req, res, next) => {
   req.session.destroy(err => {
     console.log(err);
     res.redirect('/');
+  });
+};
+
+exports.postSignup = (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const confirmPassword = req.body.confirmPassword;
+  User.findOne({ email: email })
+    .then(userDoc => {
+      if (userDoc) {
+        return res.redirect('/signup');
+      }
+      const user = new User({
+        email: email,
+        password: password,
+        cart: { items: [] }
+      });
+      return user.save();
+    })
+    .then(result => {
+      res.redirect('/login');
+    })
+    .catch(err => console.log(err));
+};
+
+exports.getSignup = (req, res, next) => {
+  res.render('auth/signup', {
+    path: '/signup',
+    pageTitle: 'Signup',
+    isAuthenticated: false
   });
 };
